812836356adccad9841bf5359f6362a7c9748def
rjames <robjames.texas@yahoo.com>
2017-07-29 19:30:16 -0500

    saving changes.

--- justTesting.py
+++ justTesting.py
@@ -31,6 +31,34 @@ bl_info = {
     "support": 'OFFICIAL',
     "category": "Import-Export"}
 
+TO_BLE_MATRIX = mathutils.Matrix([
+    [1.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 1.0, 0.0],
+    [0.0, 1.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 1.0]])
+
+def convertBranch(branch, amt, parent):
+    leaf = branch.OgreBone
+    bone = amt.edit_bones.new(leaf.Name)
+    loc = Vector((leaf.Position.X, leaf.Position.Y, leaf.Position.Z))
+    axis = Vector((leaf.Rotation.Axis.X, leaf.Rotation.Axis.Y, leaf.Rotation.Axis.Z))
+    rotmat = mathutils.Matrix.Rotation(leaf.Rotation.Angle, 4, axis)
+    
+    if parent:
+        pLoc = parent.head
+        amt.edit_bones[leaf.Name].parent = amt.edit_bones[ parent.name ]
+    else:
+        pLoc = Vector((0,0,0))
+    
+    #fLoc = ((loc * rotmat) + pLoc) * TO_BLE_MATRIX
+    fLoc = ((loc * rotmat) * TO_BLE_MATRIX) + pLoc
+    
+    bone.head = fLoc
+    #bone.tail = Vector((0,0,1))
+    bone.tail = bone.head + Vector((0,.2,0))
+    
+    for childBranch in branch.Children:
+        convertBranch(childBranch, amt, bone)
 
 if __name__ == '__main__':
     testFile = 'c:\\tmp\\Vanquisher.skeleton.xml'
@@ -46,42 +74,41 @@ if __name__ == '__main__':
     ob = bpy.context.object
     ob.show_x_ray = True
     ob.name = 'TestRig'
+    ob.draw_type = 'WIRE'
     amt = ob.data
     amt.name = 'root'+'Amt'
     amt.show_axes = True
  
-    TO_BLE_MATRIX = mathutils.Matrix([
-        [1.0, 0.0, 0.0, 0.0],
-        [0.0, 0.0, 1.0, 0.0],
-        [0.0, 1.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0, 1.0]])
-
     # Create bones
     bpy.ops.object.mode_set(mode='EDIT')
     leaf = tree.OgreBone
-    bone = amt.edit_bones.new(leaf.Name)
-    loc = Vector((leaf.Position.X, leaf.Position.Y, leaf.Position.Z))# * TO_BLE_MATRIX
-    bone.head = loc
-    axis = Vector((leaf.Rotation.Axis.X, leaf.Rotation.Axis.Y, leaf.Rotation.Axis.Z))
-    rotmat = mathutils.Matrix.Rotation(leaf.Rotation.Angle, 4, axis)
-    
-    childLeaf = tree.Children[0].OgreBone
-    cLoc = Vector((childLeaf.Position.X, childLeaf.Position.Y, childLeaf.Position.Z))
-    bone.tail = rotmat * cLoc
-    
-    childBone = amt.edit_bones.new(childLeaf.Name)
-    childBone.head = bone.tail
 
-    childAxis = Vector((childLeaf.Rotation.Axis.X, childLeaf.Rotation.Axis.Y, childLeaf.Rotation.Axis.Z))
-    childRotMat = mathutils.Matrix.Rotation(childLeaf.Rotation.Angle, 4, childAxis)
-    
-    childLeaf2 = tree.Children[0].Children[0].OgreBone
-    cLoc2 = Vector((childLeaf2.Position.X, childLeaf2.Position.Y, childLeaf2.Position.Z))
-    childBone.tail = childRotMat * cLoc2 + childBone.head
-    
-    
-    childBone2 = amt.edit_bones.new(childLeaf2.Name)
-    childBone2.head = childBone.tail
-    childAxis2 = Vector((childLeaf2.Rotation.Axis.X, childLeaf2.Rotation.Axis.Y, childLeaf2.Rotation.Axis.Z))
-    childRotMat2 = mathutils.Matrix.Rotation(childLeaf2.Rotation.Angle, 4, childAxis2)
-    childBone2.tail = childRotMat2 * Vector((0,0,1)) + childBone2.head
+    if False:
+        bone = amt.edit_bones.new(leaf.Name)
+        loc = Vector((leaf.Position.X, leaf.Position.Y, leaf.Position.Z))# * TO_BLE_MATRIX
+        bone.head = loc
+        axis = Vector((leaf.Rotation.Axis.X, leaf.Rotation.Axis.Y, leaf.Rotation.Axis.Z))
+        rotmat = mathutils.Matrix.Rotation(leaf.Rotation.Angle, 4, axis)
+        
+        childLeaf = tree.Children[0].OgreBone
+        cLoc = Vector((childLeaf.Position.X, childLeaf.Position.Y, childLeaf.Position.Z))
+        bone.tail = rotmat * cLoc
+        
+        childBone = amt.edit_bones.new(childLeaf.Name)
+        childBone.head = bone.tail
+
+        childAxis = Vector((childLeaf.Rotation.Axis.X, childLeaf.Rotation.Axis.Y, childLeaf.Rotation.Axis.Z))
+        childRotMat = mathutils.Matrix.Rotation(childLeaf.Rotation.Angle, 4, childAxis)
+        
+        childLeaf2 = tree.Children[0].Children[0].OgreBone
+        cLoc2 = Vector((childLeaf2.Position.X, childLeaf2.Position.Y, childLeaf2.Position.Z))
+        childBone.tail = childRotMat * cLoc2 + childBone.head
+        
+        
+        childBone2 = amt.edit_bones.new(childLeaf2.Name)
+        childBone2.head = childBone.tail
+        childAxis2 = Vector((childLeaf2.Rotation.Axis.X, childLeaf2.Rotation.Axis.Y, childLeaf2.Rotation.Axis.Z))
+        childRotMat2 = mathutils.Matrix.Rotation(childLeaf2.Rotation.Angle, 4, childAxis2)
+        childBone2.tail = childRotMat2 * Vector((0,0,1)) + childBone2.head
+    else:
+        convertBranch(tree, amt, False)
